5aa82a068dc1bf1ebb76174d3a6ef27f34d319ec
PashaAstrax <pashaastrax@gmail.com>
2021-10-01 15:03:17 +0300

    HW #1 is done

--- lesson1.py
+++ lesson1.py
@@ -1,12 +1,5 @@
-# ***  - вывести элемент листа, значение которого ближе всего к среднему арифметическому всех элементов этого же листа
-# пример:
-# [1, 2, 3, 4, 5, 6, 7, 8, 9] => 5
-# [-1, -2, 3, 4, 555] => 4
-# [5, 5, 5, 5] => 5
-# [-10, 5, 5] => 5
-
 # 1)Дан лист:
-#   list = [22, 3,5,2,8,2,-23, 8,23,5]
+list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
 #   - найти min число в листе
 #   - удалить все дубликаты в листе
 #   - заменить каждое четвертое значение на "Х"
@@ -17,13 +10,24 @@
 #         if i < x:
 #             x = i
 #     return x
-# print(min_number(list))
+# print("min_num: " + str(min_number(list)))
 
 # list1 = []
 # for i in list:
 #     if i not in list1:
 #         list1.append(i)
-# print(list1)
+#         list = list1
+# print(list)
+
+# arr = []
+# index = 0
+# for i in list:
+#     arr.append(index)
+#     index += 1
+#     if index % 4 == 0:
+#         i = "x"
+#     print(i, end=", ")
+
 
 
 # 2)вывести на экран пустой квадрат из "*" сторона которого указана в переменой:
@@ -50,3 +54,63 @@
 
 # 4) переделать первое задание под меню с помощью цикла
 
+# def menu():
+#     choice = input("\n==========================\n"
+#             "1. Найти min число в листе;\n"
+#             "2. Удалить все дубликаты в листе;\n"
+#             "3. Заменить каждое четвертое значение на Х;\n"
+#             "4. Вывести элемент листа, значение которого ближе всего к среднему арифметическому всех чисел;\n"
+#             "5. Выход.\n"
+#             "==========================\n"
+#             "Сделайте свой выбор: ")
+#     return int(choice)
+#
+# while True:
+#     choice = menu()
+#     if choice == 1:
+#         print(list, "\nmin_num: " + str(min(list)))
+#
+#     elif choice == 2:
+#         print(set(list))
+#
+#     elif choice == 3:
+#         arr = []
+#         index = 0
+#         for i in list:
+#             arr.append(index)
+#             index += 1
+#             if index % 4 == 0:
+#                 i = "x"
+#             print(i, end=" ")
+#
+#     elif choice == 4:
+#         def middle_num(list):
+#             a = list[::]
+#             x = sum(list) / len(list)
+#             for i, v in enumerate(list):
+#                 list[i] = abs(v - x)
+#             v = min(list)
+#             return a[list.index(v)]
+#         print(list)
+#         print(middle_num(list))
+#
+#     elif choice == 5:
+#         break
+
+
+# ***  - вывести элемент листа, значение которого ближе всего к среднему арифметическому всех элементов этого же листа
+# пример:
+# [1, 2, 3, 4, 5, 6, 7, 8, 9] => 5
+# [-1, -2, 3, 4, 555] => 4
+# [5, 5, 5, 5] => 5
+# [-10, 5, 5] => 5
+
+# def middle_num(list):
+#     a = list[::]
+#     x = sum(list) / len(list)
+#     for i, v in enumerate(list):
+#         list[i] = abs(v - x)
+#     v = min(list)
+#     return a[list.index(v)]
+#
+# print(middle_num(list))
