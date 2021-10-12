0d0429c83f5c028d0f86ed6a7bcae535b8a16e27
PashaAstrax <pashaastrax@gmail.com>
2021-10-12 20:36:44 +0300

    HW #4 is done

--- lesson4.py
+++ lesson4.py
@@ -8,44 +8,44 @@
 #   >, < меньше или больше
 #   при вызове метода len() подсчитывать сумму сторон
 
-class Rectangle:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return self.x + other.x, self.y + other.y
-
-    def __sub__(self, other):
-        return self.x - other.x, self.y - other.y
-
-    def __eq__(self, other):
-        return self.x == other.x, self.y == other.y
-
-    def __ne__(self, other):
-        return self.x != other.x, self.y != other.y
-
-    def __lt__(self, other):
-        return self.x < other.x, self.y < other.y
-
-    def __gt__(self, other):
-        return self.x > other.x, self.y > other.y
-
-    def __len__(self):
-        return self.x + self.y
-
-rectangle1 = Rectangle(3, 6)
-rectangle2 = Rectangle(4, 8)
-
-print(rectangle1 + rectangle2)
-print(rectangle1 - rectangle2)
-print(rectangle1 == rectangle2)
-print(rectangle1 != rectangle2)
-print(rectangle1 < rectangle2)
-print(rectangle1 > rectangle2)
-print(len(rectangle1))
-print(len(rectangle2))
+# class Rectangle:
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def __add__(self, other):
+#         return self.x + other.x, self.y + other.y
+#
+#     def __sub__(self, other):
+#         return self.x - other.x, self.y - other.y
+#
+#     def __eq__(self, other):
+#         return self.x == other.x, self.y == other.y
+#
+#     def __ne__(self, other):
+#         return self.x != other.x, self.y != other.y
+#
+#     def __lt__(self, other):
+#         return self.x < other.x, self.y < other.y
+#
+#     def __gt__(self, other):
+#         return self.x > other.x, self.y > other.y
+#
+#     def __len__(self):
+#         return self.x + self.y
+#
+# rectangle1 = Rectangle(3, 6)
+# rectangle2 = Rectangle(4, 8)
+#
+# print(rectangle1 + rectangle2)
+# print(rectangle1 - rectangle2)
+# print(rectangle1 == rectangle2)
+# print(rectangle1 != rectangle2)
+# print(rectangle1 < rectangle2)
+# print(rectangle1 > rectangle2)
+# print(len(rectangle1))
+# print(len(rectangle2))
 #######################################################################################################################
 # 1)Створити пустий list
 # 2)Створити клас Letter
@@ -55,38 +55,40 @@ print(len(rectangle2))
 # 6) має бути метод(метод класу) для виводу __сount
 # 7) має бути метод який записує в наш ліст текст з нашої змінної __text
 
-list1 = []
+# list1 = []
+# 
+# class Letter:
+# 
+#     __count = 0
+# 
+#     def __init__(self, text):
+#         self.__text = text
+#         # self.__class__.__count += 1
+#         Letter.__count += 1
+#         self.id = Letter.__count
+# 
+#     @classmethod
+#     def get_count(cls):
+#         print(f"We have '{cls.__count}' objects.")
+# 
+#     def __getText(self):
+#         return self.__text
+# 
+#     def __setText(self, new_value):
+#         self.__text = new_value
+#         list1.append(new_value)
+# 
+#     text = property(__getText, __setText)
 
-class Letter:
-
-    __count = 0
-
-    def __init__(self, text):
-        self.__text = text
-        self.__class__.__count += 1
-
-    @classmethod
-    def get_count(cls):
-        return (f"We have '{cls.__count}' objects.")
-
-    def __getText(self):
-        return self.__text
-
-    def __setText(self, new_value):
-        self.__text = new_value
-        list1.append(new_value)
-
-    text = property(__getText, __setText)
-
-letter = Letter("")
-letter1 = Letter("")
-letter2 = Letter("")
-print(Letter.get_count())
-letter.text = "new text"
-letter1.text = "some text"
-letter2.text = "some new text"
-print(letter.text, letter1.text, letter2.text)
-print(list1)
+# letter = Letter("")
+# letter1 = Letter("")
+# letter2 = Letter("")
+# Letter.get_count()
+# letter.text = "new text"
+# letter1.text = "some text"
+# letter2.text = "some new text"
+# print(letter.text, letter1.text, letter2.text)
+# print(list1)
 #######################################################################################################################
 # создать класс Human (name, age)
 # создать два класса Prince и Cinderella:
@@ -96,46 +98,112 @@ print(list1)
 # класса золушки должна быть переменная count которая будет считать сколько экземпляров класса золушка было создано
 # и метод класса который будет показывать это количество
 
-class Human:
+# class Human:
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# class Cinderella(Human):
+#     __count = 0
+#
+#     def __init__(self, name, age, footSize):
+#         super().__init__(name, age)
+#         self.footSize = footSize
+#         self.__class__.__count += 1
+#
+#     @classmethod
+#     def get_count(cls):
+#         return (f"We have << {cls.__count} >> objects of cinderallas")
+#
+#     def __str__(self):
+#         return str(self.__dict__)
+#
+#     def __repr__(self):
+#         return self.__str__()
+#
+# class Prince(Human):
+#
+#     def __init__(self, name, age, shoes):
+#         super().__init__(name, age)
+#         self.shoes = shoes
+#
+#     def find(self, cinderellas):
+#         for cinderella in cinderellas:
+#             if cinderella.footSize == prince.shoes:
+#                 return (f"Our Cinderella is '{cinderella.name}'")
+#
+#
+# cinderellas = [Cinderella("Anna", 18, 35),
+#                Cinderella("Maria", 19, 36),
+#                Cinderella("Katy", 20, 37)]
+# prince = Prince("John", 22, 37)
+#
+# print(Cinderella.get_count())
+# print(prince.find(cinderellas))
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+class Notebook:
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self):
+        self.jotter = {}
 
-class Cinderella(Human):
-    __count = 0
+    def add(self, key, val):
+        self.jotter[key] = val
+        return "\'Запись создана\'"
 
-    def __init__(self, name, age, footSize):
-        super().__init__(name, age)
-        self.footSize = footSize
-        self.__class__.__count += 1
+    def find(self, name):
+        if name in self.jotter:
+            data = ' '.join(self.jotter[name])
+            return f"name: '{name.capitalize()}', price: {data}$"
+        return "\'Покупка не найдена\'"
 
-    @classmethod
-    def get_count(cls):
-        return (f"We have << {cls.__count} >> objects of cinderallas")
+    def info(self):
+        return self.jotter.items()
 
-    def __str__(self):
-        return str(self.__dict__)
-
-    def __repr__(self):
-        return self.__str__()
-
-class Prince(Human):
-
-    def __init__(self, name, age, shoes):
-        super().__init__(name, age)
-        self.shoes = shoes
-
-    def find(self, cinderellas):
-        for cinderella in cinderellas:
-            if cinderella.footSize == prince.shoes:
-                return (f"Our Cinderella is '{cinderella.name}'")
+    def sum_price(self):
+        return self.jotter.items()
 
 
-cinderellas = [Cinderella("Anna", 18, 35),
-               Cinderella("Maria", 19, 36),
-               Cinderella("Katy", 20, 37)]
-prince = Prince("John", 22, 37)
+    def delete(self, name):
+        if name in self.jotter:
+            del self.jotter[name]
+            return f'"{name}" удален'
+        return "\'Нет такого в списке\'"
 
-print(Cinderella.get_count())
-print(prince.find(cinderellas))
+print('''\nВыберите что вы хотите сделать:
+    * 1 - Создать запись
+    * 2 - Поиск по названию покупки
+    * 3 - Список все записей
+    * 4 - Удалить запись
+    * 5 - выход''')
+
+book = Notebook()
+while True:
+
+    command = input("\nВведите команду: ")
+
+    if command == '1':
+        name = input("Введите название покупки: ")
+        price = input("Введите цену покупки: ")
+        print(book.add(name, [price]))
+
+    elif command == '2':
+        name = input("Чтобы найти, введите название покупки: ")
+        print(book.find(name))
+
+    elif command == '3':
+        for name, data in book.info():
+            data = ' '.join(data)
+            print(f"name: '{name.capitalize()}', price: {data}$")
+
+    elif command == '4':
+        name = input("Введите покупку которую хотите удалить:  ")
+        print(book.delete(name))
+
+    elif command == '5':
+        break
+
+    else:
+        print('Неизвестная команда')
